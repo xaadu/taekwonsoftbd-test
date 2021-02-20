840d5d8d27348effeaccef8b41b89dcffc56bea4
xaadu <xayed42@gmail.com>
2021-02-20 14:47:05 +0600

    Fixed certificate not found issue

--- proxy/shell/init_certbot_staging.sh
+++ proxy/shell/init_certbot_staging.sh
@@ -1,4 +1,4 @@
 !/bin/sh
-#rm -rf /etc/letsencrypt
+rm -rf /etc/letsencrypt/live/generatedcert
 certbot certonly --webroot -w /usr/share/nginx/html --email $REGISTER_EMAIL --non-interactive --agree-tos --cert-name generatedcert --domains $DOMAINS --staging
 nginx -s reload
