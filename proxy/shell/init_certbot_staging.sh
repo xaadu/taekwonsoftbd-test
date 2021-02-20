744088a7b449228625c41838314f12465be01e86
xaadu <xayed42@gmail.com>
2021-02-20 15:15:00 +0600

    .

--- proxy/shell/init_certbot_staging.sh
+++ proxy/shell/init_certbot_staging.sh
@@ -1,4 +1,4 @@
-/bin/ash
-rm -rf /etc/letsencrypt/live/generatedcert
+#!/bin/ash
+rm -rf /etc/letsencrypt/live
 certbot certonly --webroot -w /usr/share/nginx/html --email $REGISTER_EMAIL --non-interactive --agree-tos --cert-name generatedcert --domains $DOMAINS --staging
 nginx -s reload
